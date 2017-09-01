ab722beade55dbeeac64a394456be107161530bf
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-09-01 16:20:30 -0300

    fix: format.float() now works on vectors of values

--- R/format.float.R
+++ R/format.float.R
@@ -1,5 +1,4 @@
 format.float <- function(value, digits = 2) {
-  if (any(is.na(value), is.null(value), is.nan(value))) return(NA)
   value <- formatC(value, format = "f", digits = digits)
   value
 }
