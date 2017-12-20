fb0df539a151ec43125d817c58f0e983d7c511f1
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2017-12-20 11:26:35 -0200

    replace 0 by NA, instead of ignoring it
    This is in order to keep consistency with the current usage

--- R/geomean.R
+++ R/geomean.R
@@ -1,4 +1,4 @@
 geomean <- function(datavector) {
-  datavector <- datavector[!(datavector == 0)] # discard null values
+  datavector[(datavector == 0)] <- NA # discard null values
   10^mean(log10(datavector), na.rm = T)
 }
