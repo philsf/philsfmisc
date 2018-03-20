bcaf695428a019f907334acab835b2433e69df47
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2018-03-20 15:31:10 -0300

    we don't need na.rm anymore
    The NA values are removed in the fist line

--- R/predint.R
+++ R/predint.R
@@ -1,9 +1,9 @@
 predint <- function(datavector, alpha = 0.05) {
   datavector <- datavector[!is.na(datavector)] # discard NA values
   N <- length(datavector)
-  xbar <- mean(datavector, na.rm = na.rm) # X bar
+  xbar <- mean(datavector) # X bar
   ts <- qt(1-alpha/2, N-1) # student's t with N - 1 degrees of freedom
-  sdl <- sd(datavector, na.rm = na.rm) # SD of log titers
+  sdl <- sd(datavector) # SD of log titers
   sq <- sqrt( 1 + 1/N )  # square root term
   range <- c(xbar - ts*sdl*sq, xbar + ts*sdl*sq) # formula
   range
