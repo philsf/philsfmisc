e90982301042fa543275a418438820435bf12391
Felipe Figueiredo <felipe.figueiredo@bio.fiocruz.br>
2018-05-11 10:58:05 -0300

    include % symbol by default

--- R/format.pct.R
+++ R/format.pct.R
@@ -1,3 +1,5 @@
-format.pct <- function(p, digits = 0) {
-  format.float(p*100, digits)
+format.pct <- function(p, digits = 0, pct.symbol = TRUE) {
+  symbol <- "%"
+  if (!pct.symbol) symbol <- NULL
+  paste0(format.float(p*100, digits), symbol)
 }
