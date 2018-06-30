a4e7b3a9cd8e62f5194525e4604414682c26ed1e
Felipe Figueiredo <phisf79@gmail.com>
2018-06-30 20:17:57 -0300

    default to 1 digit for percentages

--- R/format.pct.R
+++ R/format.pct.R
@@ -1,4 +1,4 @@
-format.pct <- function(p, digits = 0, pct.symbol = TRUE) {
+format.pct <- function(p, digits = 1, pct.symbol = TRUE) {
   symbol <- "%"
   if (!pct.symbol) symbol <- NULL
   paste0(format.float(p*100, digits), symbol)
