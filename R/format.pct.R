58d139a462f1f95679010d29d79ce43a5deccb7d
Felipe Figueiredo <philsf79@gmail.com>
2018-10-15 20:23:13 -0300

    roxygen comments for format.pct

--- R/format.pct.R
+++ R/format.pct.R
@@ -1,3 +1,19 @@
+#' Convenience function to format proportions as percentages
+#'
+#' @param p a numeric vector assumed to be a proportion.
+#' @param digits the number of decimal digits to be used in the output.
+#' @param pct.symbol (logical) whether or not to include the percent sign in the output.
+#'
+#' @return A character value formatted as percentages
+#'
+#' @examples
+#' format.pct(.1)
+#' format.pct(c(.42, .99), 0)
+#'
+#' m <- 10*matrix(c(1, 1.1, .9, 2), nrow = 2)
+#' format.pct(fisher.test(m)$p.value)
+#' format.pct(fisher.test(5*m)$p.value)
+#' format.pct(fisher.test(m)$conf.int)
 #'
 #' @author Felipe Figueiredo
 #' @export
