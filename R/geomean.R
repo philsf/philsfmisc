5480d57c00ccadbdb17191d85f28b754b4dc2182
Felipe Figueiredo <philsf79@gmail.com>
2018-10-15 20:22:51 -0300

    roxygen comments for geomean

--- R/geomean.R
+++ R/geomean.R
@@ -1,3 +1,12 @@
+#' Compute the geometric mean
+#'
+#' @param x vector of numeric.
+#' @details If any null values are present in \code{x} they are first replaced with \code{NA}.
+#' @return Returns the geometric mean of \code{x}.
+#' @examples
+#' d <- c(10,100,1000)
+#' geomean(d)
+#' geomean(c(d,0)) # na.rm is TRUE by default for sd()
 #'
 #' @author Felipe Figueiredo
 #' @export
