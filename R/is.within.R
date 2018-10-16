c3ea6d55dc73c5285f4bb67e06a5faae86fb0ce8
Felipe Figueiredo <philsf79@gmail.com>
2018-10-15 21:06:51 -0300

    roxygen tags for is.within

--- R/is.within.R
+++ R/is.within.R
@@ -1,18 +1,23 @@
+#' Check if number is within given interval
 #'
 #' @description
 #'
-#' @param value
-#' @param interval
-#' @param open.interval
+#' @param value A numeric value, vector or list.
+#' @param interval The (numeric) interval within which \code{value} is being tested.
+#' @param open.interval logical: should \code{interval} be regarded as an open interval?
 #'
-#' @return
+#' @return The default method for \code{is.within} applied to an atomic vector returns
+#'  a logical vector of the same length as its argument x, containing TRUE for those elements contained within \code{interval}, and FALSE otherwise.
 #'
 #' @examples
+#' is.within(1.5, c(0,5)) # TRUE
+#' is.within(6, c(0,5)) # FALSE
 #'
-#'
+#' is.within(c(1.5, 6), c(0,5)) # vector: TRUE, TRUE
+#' is.within(list(1.5, 6), c(0,5)) # list: TRUE, FALSE
 #'
 #' @author Felipe Figueiredo
-#' @export
+#' @export is.within
 
 is.within <- function(value, interval, open.interval = FALSE) {
   if (open.interval) return( value > interval[1] & value < interval[2] )
