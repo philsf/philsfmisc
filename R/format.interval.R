799bb23c9ac18ce1c2432f51f3ad693ecd5765e0
Felipe Figueiredo <philsf79@gmail.com>
2018-10-22 20:34:14 -0300

    doc fix in format.interval (clarity)

--- R/format.interval.R
+++ R/format.interval.R
@@ -3,8 +3,8 @@
 #' \code{format.interval} prepares intervals for results printing.
 #'
 #' This function formats a given interval with the given number of decimal digits.
-#'   Numbers are always assumed to be \code{\link[base]{double}}.
-#'   If there is a percent sign in \code{x}, it is removed prior to formatting by \code{format.float}.
+#'   \code{interval} is always assumed to be a two valued vector of \code{\link[base]{double}}.
+#'   If there is a percent sign in any of \code{interval} values, it is removed prior to formatting by \code{format.float}.
 #'
 #' @param interval Must be a vector of two values.
 #'   Values can be either numeric or character.
